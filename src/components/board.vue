4374f6b6881395ebb13b6253dcb6ffecf70ca08b
Janine Hempy <jhempy@gmail.com>
2017-10-12 13:28:37 -0400

    Initial commit

---
+++ src/components/board.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div class="board">
+  
+    <card></card>
+
+  </div>
+
+</template>
+
+<script>
+import card from './card.vue';
+
+export default {
+  name: 'board',
+  components: { card },
+  data: function() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+
+}
+
+</script>
+
+<style>
+
+.board {
+  border: 1px solid #ccc;
+}
+
+</style>
---
+++ src/components/card.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <p>This is a card</p>
+
+</template>
+
+<script>
+
+export default {
+  name: 'card',
+  data: function() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+
+
+}
+
+</script>
+
+<style>
+
+</style>
